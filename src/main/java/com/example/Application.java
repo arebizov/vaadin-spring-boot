460c77c4f7fbbcb9e734a7e95f0301e37ec223ee
arebizov <42617009+arebizov@users.noreply.github.com>
2018-08-22 20:14:22 +0300

    Added war packaging

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +15,13 @@ import java.util.List;
  * Created by MikhalevPV on 25.04.2018.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
